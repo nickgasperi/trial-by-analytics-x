ef26352df7d97a683a7b30c70ed4f9e497ff76a8
Nick Gasperi <nickgasperi5@gmail.com>
2025-02-16 11:41:59 -0600

    Create X03-11.24.24-qb_epa_2024.R
    Change file ext.

---
+++ X03-11.24.24-qb_epa_2024.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+library(nflfastR)
+library(nflplotR)
+nflreadr::clear_cache()
+###
+data24 = load_pbp(2024)
+###
+passing24 = data24%>%
+  filter(week < 12, play_type == "pass", !is.na(air_yards))
+###
+qbepa = passing24 %>%
+  filter(!is.na(epa))%>%
+  group_by(passer,
+           team = posteam)%>%
+  summarize(att = n(),
+            epa = mean(epa),
+            .groups = "drop")%>%
+  filter(att > 158)%>%
+  arrange(att)%>%
+  print(n = Inf)
+##
+plotqbepa1 = ggplot(data = qbepa, aes(x = epa, y = reorder(passer,epa)))+
+  geom_col(aes(color = team, fill = team), width = .6)+
+  scale_color_nfl(type = "secondary")+
+  scale_fill_nfl(alpha = .7)+
+  geom_nfl_logos(aes(team_abbr = team), width = 0.033, alpha = 0.75)+
+  labs(title = "2024 QB EPA Per Attempt",
+       subtitle = "Thru Week 11 (min. 158 att.)",
+       x = "EPA Per Attempt",
+       y = "",
+       caption = "By Nick Gasperi | Data @nflfastR")+
+  theme_bw()+
+  theme(plot.title = element_text(face = "bold", hjust = 0.5, size =15),
+        plot.subtitle = element_text(hjust = 0.5, size = 10),
+        axis.text = element_text(face = "bold"),
+        axis.title = element_text(face = "bold", size = 10))
+plotqbepa1
+
+
+# + expand limits(x = -0.45)
