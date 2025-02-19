a19891554578b76d5a5f4b110fbd5432ebbb8290
Nick Gasperi <nickgasperi5@gmail.com>
2025-02-18 22:17:52 -0600

    Update X03-11.24.24-qb_epa_2024.R

--- X03-11.24.24-qb_epa_2024.R
+++ X03-11.24.24-qb_epa_2024.R
@@ -1,24 +1,28 @@
+# load packages
 library(tidyverse)
+library(ggplot2)
 library(nflfastR)
 library(nflplotR)
-nflreadr::clear_cache()
-###
+
+# load data
 data24 = load_pbp(2024)
-###
-passing24 = data24%>%
-  filter(week < 12, play_type == "pass", !is.na(air_yards))
-###
-qbepa = passing24 %>%
-  filter(!is.na(epa))%>%
+
+# filter data
+qbepa = data24%>%
+  filter(week < 12,
+         play_type == "pass",
+         !is.na(air_yards),
+         !is.na(epa)) %>% 
   group_by(passer,
-           team = posteam)%>%
+           team = posteam) %>%
   summarize(att = n(),
             epa = mean(epa),
-            .groups = "drop")%>%
-  filter(att > 158)%>%
-  arrange(att)%>%
+            .groups = "drop") %>%
+  filter(att > 158) %>%
+  arrange(att) %>%
   print(n = Inf)
-##
+
+# plot data
 plotqbepa1 = ggplot(data = qbepa, aes(x = epa, y = reorder(passer,epa)))+
   geom_col(aes(color = team, fill = team), width = .6)+
   scale_color_nfl(type = "secondary")+
@@ -34,7 +38,8 @@ plotqbepa1 = ggplot(data = qbepa, aes(x = epa, y = reorder(passer,epa)))+
         plot.subtitle = element_text(hjust = 0.5, size = 10),
         axis.text = element_text(face = "bold"),
         axis.title = element_text(face = "bold", size = 10))
+
+# view plot
 plotqbepa1
 
 
-# + expand limits(x = -0.45)
