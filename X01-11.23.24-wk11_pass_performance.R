9c3e9f00c73506f1d08f7e5282593bf84584e727
Nick Gasperi <nickgasperi5@gmail.com>
2025-03-07 19:44:09 -0600

    Update X01-11.23.24-wk11_pass_performance.R

--- X01-11.23.24-wk11_pass_performance.R
+++ X01-11.23.24-wk11_pass_performance.R
@@ -12,15 +12,15 @@ pbp24 = load_pbp(2024)
 wk11_24_pass_air_epa = pbp24 %>%
   filter(week == 11,
           play_type == "pass",
-          !is.na(air_yards)) %>%    # include only passes from week 11   
+          !is.na(air_yards)) %>%            # include only passes from week 11   
   group_by(id, name) %>%
   summarize(team = last(posteam),
             att = n(),
             ydair = mean(air_yards),
             epa = mean(epa)) %>%
-  filter(att > 15) %>%              # include only players with more than 15 pass attempts
-  arrange(epa) %>%                  # use arrange to sort tibble
-  print(n = Inf)                    # print all rows
+  filter(att > 15) %>%                      # include only players with more than 15 pass attempts
+  arrange(epa) %>%                          # use arrange to sort tibble
+  print(n = Inf)                            # print all rows
 
 # plot week 11 QB performance
 plot221 = ggplot(data = wk11_24_pass_air_epa,
@@ -29,12 +29,12 @@ plot221 = ggplot(data = wk11_24_pass_air_epa,
        subtitle = "Aggressiveness vs. Effectiveness",
        x = "Air Yards Per Attempt",
        y = "EPA Per Attempt") +
-  geom_text_repel(aes(label = wk11_24_pass_air_epa$name)) +       # replaces geom_text
-  geom_nfl_logos(aes(team_abbr = team), width = .05)+             # add player team logo to data points
+  geom_text_repel(aes(label = wk11_24_pass_air_epa$name)) +         # replaces geom_text
+  geom_nfl_logos(aes(team_abbr = team), width = .05)+               # add player team logo to data points
   geom_hline(yintercept = mean(wk11_24_pass_air_epa$epa),
-             linetype = "dashed") +                               # plot avg epa
+             linetype = "dashed") +                                 # plot avg epa
   geom_vline(xintercept = mean(wk11_24_pass_air_epa$ydair),
-             linetype = "dashed") +                               # plot avg air yards
+             linetype = "dashed") +                                 # plot avg air yards
   theme_bw() +
   theme(plot.title = element_text(face="bold", hjust = 0.5, size = 17),
         plot.subtitle = element_text(hjust = 0.5, size = 12))
