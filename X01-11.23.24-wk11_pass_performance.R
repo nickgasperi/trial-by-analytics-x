51b626b6c79c209f0cc73eb88f392614a7f31eb0
Nick Gasperi <nickgasperi5@gmail.com>
2025-03-07 19:25:41 -0600

    Update X01-11.23.24-wk11_pass_performance.R

--- X01-11.23.24-wk11_pass_performance.R
+++ X01-11.23.24-wk11_pass_performance.R
@@ -1,16 +1,15 @@
 # load packages
-library(tidyverse)
-library(ggrepel)
-library(nflreadr)
+library(tidyverse)   # data wrangling
+library(ggrepel)     # replaces geom_text
+library(nflreadr)    
 library(nflfastR)
 library(nflplotR)
 
-# load data
+# load 2024 NFL data
 pbp24 = load_pbp(2024)
 
 # filter data to include only passing plays from Week 11
 # calculate averages
-# filter to see passers with over 15 attempts
 wk11_24_pass_air_epa = pbp24 %>%
   filter(week == 11,
           play_type == "pass",
@@ -20,23 +19,25 @@ wk11_24_pass_air_epa = pbp24 %>%
             att = n(),
             ydair = mean(air_yards),
             epa = mean(epa)) %>%
-  filter(att > 15) %>%
+  filter(att > 15) %>%              # include only players with more than 15 pass att
   arrange(epa) %>%
   print(n = Inf)
 
 # use ggplot to create plot
-# use text repel to reduce data label overlapping with aesthetics to add player name for each data point
 # geom_nfl_logos  adds player's team logo to each data point
 # use geom hline and vline to plot averages for each axis
-plot221 = ggplot(data = wk11_24_pass_air_epa, aes(x = ydair, y = epa)) +
+plot221 = ggplot(data = wk11_24_pass_air_epa,
+                 aes(x = ydair, y = epa)) +
   labs(title = "2024 Week 11 Passing Performance",
        subtitle = "Aggressiveness vs. Effectiveness",
        x = "Air Yards Per Attempt",
        y = "EPA Per Attempt") +
-  geom_text_repel(aes(label = wk11_24_pass_air_epa$name)) +
-  geom_nfl_logos(aes(team_abbr = team), width = .05)+
-  geom_hline(yintercept = mean(wk11_24_pass_air_epa$epa), linetype = "dashed") +
-  geom_vline(xintercept = mean(wk11_24_pass_air_epa$ydair), linetype = "dashed") +
+  geom_text_repel(aes(label = wk11_24_pass_air_epa$name)) +       # replaces geom_text
+  geom_nfl_logos(aes(team_abbr = team), width = .05)+             # add player team logo to data
+  geom_hline(yintercept = mean(wk11_24_pass_air_epa$epa),
+             linetype = "dashed") +
+  geom_vline(xintercept = mean(wk11_24_pass_air_epa$ydair),
+             linetype = "dashed") +
   theme_bw() +
   theme(plot.title = element_text(face="bold", hjust = 0.5, size = 17),
         plot.subtitle = element_text(hjust = 0.5, size = 12))
