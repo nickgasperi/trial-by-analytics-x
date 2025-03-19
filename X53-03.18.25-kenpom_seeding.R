7ad982400872d01a98285bdec5d72d12b57e35ce
Nick Gasperi <nickgasperi5@gmail.com>
2025-03-18 22:38:14 -0500

    Create X53-03.18.25-kenpom_seeding.R

---
+++ X53-03.18.25-kenpom_seeding.R
@@ -0,0 +1,22 @@
+library(tidyverse)        # data wrangling
+library(readxl)           # to import data
+library(PerformanceAnalytics)     # to plot relationships among variables
+
+# convert 2025 NCAA CBB dataframe to tibble
+kenpom2 = as_tibble(kenpom1)
+
+# view tibble
+kenpom2
+
+# define numeric variables
+kenpomNUM = kenpom2 %>%
+  filter(!is.na(Seed)) %>%    # include only tournament teams
+  select(Rk, Seed, Wins, Losses, NetRtg, ORtg, DRtg, AdjT, Luck,
+         `opp-NetRtg`, `opp-ORtg`, `opp-DRtg`, `noncon-NetRtg`)
+
+# plot correlation chart
+corplot1 = chart.Correlation(kenpomNUM,
+                             pch = 19,
+                             cex.labels = 1.5)
+
+# I manually cropped the final image to include only the first two rows
